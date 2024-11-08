31ce5b06d137d43eba7f47a689250e279819ff24
fredeucn <150657537+fredeucn@users.noreply.github.com>
2024-11-08 11:00:38 +0100

    add individual car page

--- KoreklarMVC/KoreklarMVC/obj/Debug/net8.0/KoreklarMVC.AssemblyInfo.cs
+++ KoreklarMVC/KoreklarMVC/obj/Debug/net8.0/KoreklarMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoreklarMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72a94e6f15270c5f06087da88c55cde5f8808c14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7363e8bc8d5bbeea0f0d63256bdcdc2d8f154ffe")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoreklarMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoreklarMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
