cf5213ff04aa2f60b22037530c00d0fdb7d9d761
ChristianNissen <150657466+ChristianNissen@users.noreply.github.com>
2024-11-28 12:24:52 +0100

    Changes

--- KoreklarMVC/KoreklarMVC/obj/Debug/net8.0/KoreklarMVC.AssemblyInfo.cs
+++ KoreklarMVC/KoreklarMVC/obj/Debug/net8.0/KoreklarMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoreklarMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2009c94386eb23fc73b102e16224ba97c726d59b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+952fff8245d8d29fa1df0aa2809632f13cfbd7f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoreklarMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoreklarMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
